b7b080723ec3b844069a41323dc47e2f39f09452
Pedro Insausti <pedro@pedro-i5>
2023-03-29 19:53:02 -0300

    prueba2

--- src/Redux/reducer/index.js
+++ src/Redux/reducer/index.js
@@ -1,5 +1,5 @@
 import image1 from '../../foto.jpg'
-import image3 from '../../certificado.jpg'
+import image3 from '../../certificado2.jpg'
 const initialState = {
  cards: [{image: image1, title: "Pedro Insausti", description: " Soy Fullstack developer y bienvenidos a mi portfolio, donde podran conocer algunas de mis skills, mis proyectos y parte de mi vida personal."},{title: "¿Qué significa Henry? ", image: image3, description:"Henry es un bootcamp que me dio herramientas, conocimientos y me intrudujo al mundo IT haciendome conocer una comunidad extraordinaria con una gran calidad humana. Significó un gran paso en mi vida "}]
  };
